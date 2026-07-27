9b115ddb7fa687ee74d5f034f3280feb71bf1a65
Aryan <bhattiaryan777@gmail.com>
2026-07-27 23:02:31 +0530

    add task done

---
+++ script.js
@@ -0,0 +1,57 @@
+const nav_btn = document.querySelector(".nav-btn");
+const form_div = document.querySelector(".form");
+const form = document.querySelector("form");
+const nav_btn_cancel = document.querySelector(".cancel")
+
+
+nav_btn.addEventListener("click", () => {
+    form_div.style.display = "flex";
+    nav_btn_cancel.style.display = "inline"
+}) 
+
+nav_btn_cancel.addEventListener("click", () => {
+    form_div.style.display = "none",
+    nav_btn_cancel.style.display = "none"
+})
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+        let task = e.target[0].value.trim()
+        let description = e.target[1].value.trim()
+        let category = e.target[2].value
+
+        if(task === "" || description === ""){
+            alert("All fields are mandatory")
+            return
+        }
+
+        const newTask = {
+            id: Date.now(),
+            task, 
+            description,
+            category,
+            isCompleted: false
+        }
+
+        const arr = JSON.parse(localStorage.getItem("tasks"));
+
+        arr.push(newTask)
+
+        localStorage.setItem("tasks", JSON.stringify(arr));
+
+        e.target[0].value = ""
+
+        e.target[1].value = ""
+
+        showTasks()
+
+        
+})
+
+const showTasks = () => {
+    const tasks = JSON.parse(localStorage.getItem("tasks"))
+console.log(tasks)
+
+}
+
+showTasks()
