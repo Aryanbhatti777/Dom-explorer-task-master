9469f87724579b70e7f280a4d80d32d0bb154748
Aryan <bhattiaryan777@gmail.com>
2026-07-28 00:35:06 +0530

    show task with styling done

--- script.js
+++ script.js
@@ -2,55 +2,86 @@ const nav_btn = document.querySelector(".nav-btn");
 const form_div = document.querySelector(".form");
 const form = document.querySelector("form");
 const nav_btn_cancel = document.querySelector(".cancel")
-
+const tasks_div = document.querySelector(".tasks")
+const text = document.querySelector(".no-tasks")
 
 nav_btn.addEventListener("click", () => {
     form_div.style.display = "flex";
     nav_btn_cancel.style.display = "inline"
-}) 
+})
 
 nav_btn_cancel.addEventListener("click", () => {
     form_div.style.display = "none",
-    nav_btn_cancel.style.display = "none"
+        nav_btn_cancel.style.display = "none"
 })
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-        let task = e.target[0].value.trim()
-        let description = e.target[1].value.trim()
-        let category = e.target[2].value
+    let task = e.target[0].value.trim()
+    let description = e.target[1].value.trim()
+    let category = e.target[2].value;
 
-        if(task === "" || description === ""){
-            alert("All fields are mandatory")
-            return
-        }
+    if (task === "" || description === "") {
+        alert("All fields are mandatory")
+        return
+    }
 
-        const newTask = {
-            id: Date.now(),
-            task, 
-            description,
-            category,
-            isCompleted: false
-        }
+    const newTask = {
+        id: Date.now(),
+        task,
+        description,
+        category,
+        isCompleted: false
+    }
 
-        const arr = JSON.parse(localStorage.getItem("tasks"));
+    const arr = JSON.parse(localStorage.getItem("tasks")) || [];
 
-        arr.push(newTask)
+    arr.push(newTask)
 
-        localStorage.setItem("tasks", JSON.stringify(arr));
+    localStorage.setItem("tasks", JSON.stringify(arr));
 
-        e.target[0].value = ""
+    e.target[0].value = ""
 
-        e.target[1].value = ""
+    e.target[1].value = ""
 
-        showTasks()
+    showTasks()
 
-        
 })
 
 const showTasks = () => {
-    const tasks = JSON.parse(localStorage.getItem("tasks"))
-console.log(tasks)
+    const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+    // console.log(tasks)
+
+    tasks.forEach(item => {
+
+        const element = document.createElement("div")
+
+        element.classList.add("task");
+
+        element.innerHTML += `<div class="task">
+                <div class="task-top">
+                    <h1>${item.task}</h1>
+                    <p>${item.category}</p>
+                    
+                </div>
+                <p>${item.description}</p>
+                <div>
+                    <button class="complete">Complete</button>
+                    <button class="edit">Edit</button>
+                    <button class="delete">Delete</button>
+                </div>
+            </div>`
+
+
+
+
+        tasks_div.append(element)
+
+
+
+    });
+
 
 }
 
